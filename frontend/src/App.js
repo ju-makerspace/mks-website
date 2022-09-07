8ccce8491bb011142fb8bf8e42f3e88675a4bc40
Arpit Verma <arpitv970@gmail.com>
2022-09-07 12:46:53 +0530

    Added React-Router DOM

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './components/About';
+import Mentors from './components/Mentors';
+import Featured from './components/Featured';
 
 const App = () => {
     return (
-        <div className='App'>
-            <Navbar />
-            <Home />
-        </div>
+        <BrowserRouter>
+            <div className='App'>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path='/about' element={<About />} />
+                    <Route path='/mentors' element={<Mentors />} />
+                    <Route path='/featured' element={<Featured />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 };
 
---
+++ frontend/src/components/Featured.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Featured = () => {
+  return (
+    <div>
+        <h1>This is Featured page</h1>
+    </div>
+  )
+}
+
+export default Featured
