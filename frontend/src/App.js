50d717cb07afb630d84e2802555a9df3d18e6df5
Arpit Verma <arpitv970@gmail.com>
2022-09-20 21:07:51 +0530

    Added sample footer & made some changes with CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import About from './components/About';
 import Mentors from './components/Mentors';
 import Featured from './components/Featured';
+import TypeForm from './components/TypeForm';
+import Footer from './components/Footer';
 
 const App = () => {
     return (
         <BrowserRouter>
             <div className='App'>
                 <Navbar />
+                
                 <Routes>
-                    <Route path="/" element={<Home />} />
+                    <Route path='/' element={<Home />} />
                     <Route path='/about' element={<About />} />
                     <Route path='/mentors' element={<Mentors />} />
                     <Route path='/featured' element={<Featured />} />
+                    <Route path='/join-now' element={<TypeForm />} />
                 </Routes>
+                <Footer />
             </div>
         </BrowserRouter>
     );
