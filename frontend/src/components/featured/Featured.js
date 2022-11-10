427a723c4856a4aca32b30a766de28802117d3c4
Arpit Verma <arpitv970@gmail.com>
2022-11-10 21:26:23 +0530

    made more components inside Featured section

--- frontend/src/components/featured/Featured.js
+++ frontend/src/components/featured/Featured.js
@@ -1,5 +1,5 @@
 import '../../styles/desktop/featured/Featured.css';
-import { TwitterTimelineEmbed, TwitterTweetEmbed } from 'react-twitter-embed';
+import Covid from './Covid';
 
 const Featured = () => {
     return (
@@ -8,28 +8,8 @@ const Featured = () => {
                 <p className='featured-heading'>
                     Let's talk about places we got <span>Featured</span> ✨
                 </p>
-
-                <div className='featured'>
-                    <h1 className='featured_title'>Covid-19 Face shield</h1>
-                    <p className='featured_desc featured-item'>
-                        Amid Pandemic JU Maker Space launched{' '}
-                        <b>#Save_The_Saviours Initiative</b> which was applauded
-                        by <b>Hon'ble CM of Rajasthan, Sh. Ashok Gehlot</b> on
-                        his social media handles. In the initiative JU
-                        Makerspace had committed itself towards manufacturing{' '}
-                        <b>5000+ face shields</b> for hospitals of Delhi NCR to
-                        help our doctors, paramedics & sanitation workers so
-                        that they protect them from infection as they fight it
-                        out on the frontline.
-                    </p>
-                    <div className='featured-item'>
-                        <div className='featured-proof'>
-                            <TwitterTweetEmbed tweetId='1248130891118825472' />
-                            <img src='https://jecrcuniversity.edu.in/umakerspace12345/Page-8-Image-12.jpg?width=400' />
-                            <img src='https://jecrcuniversity.edu.in/umakerspace12345/Page-5-Image-5.jpg' />
-                        </div>
-                    </div>
-                </div>
+                <Covid />
+                <Covid />
             </section>
         </div>
     );
