178f8f5a5125f940f202a440632174fd6bbbbb0c
Arpit Verma <arpitv970@gmail.com>
2022-11-24 00:26:29 +0530

    add react spinners

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,24 +9,48 @@ import TypeForm from './components/TypeForm';
 import Footer from './components/Footer';
 import Alumni from './components/alumni/Alumni';
 import ScrollToTop from './components/ScrollToTop';
+import HashLoader from 'react-spinners/HashLoader';
+import { useEffect, useState } from 'react';
 
 const App = () => {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000);
+    }, []);
     return (
         <BrowserRouter>
-        <ScrollToTop />
-            <div className='App'>
-                <Navbar />
+            {loading ? (
+                <div className='spinner'>
+                    <HashLoader
+                        color='#1a7dff'
+                        loading={loading}
+                        size={50}
+                        aria-label='Loading Spinner'
+                        data-testid='loader'
+                    />
+                </div>
+            ) : (
+                <>
+                    <ScrollToTop />
+                    <div className='App'>
+                        <Navbar />
 
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/about' element={<About />} />
-                    <Route path='/mentors' element={<Mentors />} />
-                    <Route path='/featured' element={<Featured />} />
-                    <Route path='/alumni' element={<Alumni />} />
-                    <Route path='/join-now' element={<TypeForm />} />
-                </Routes>
-            </div>
-            <Footer />
+                        <Routes>
+                            <Route path='/' element={<Home />} />
+                            <Route path='/about' element={<About />} />
+                            <Route path='/mentors' element={<Mentors />} />
+                            <Route path='/featured' element={<Featured />} />
+                            <Route path='/alumni' element={<Alumni />} />
+                            <Route path='/join-now' element={<TypeForm />} />
+                        </Routes>
+                    </div>
+                    <Footer />
+                </>
+            )}
         </BrowserRouter>
     );
 };
