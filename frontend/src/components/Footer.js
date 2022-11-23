3568d4dbe499b369c77d0fc25b1451181f72bf31
Arpit Verma <arpitv970@gmail.com>
2022-11-23 11:38:30 +0530

    Improvement in Navbar

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,50 +1,85 @@
 import '../styles/desktop/Footer.css';
+import { IoMailSharp } from 'react-icons/io5';
+import { SiInstagram, SiGithub, SiTwitter } from 'react-icons/si';
+import { TfiLinkedin } from 'react-icons/tfi';
+import { IconContext } from 'react-icons/lib';
 
 const Footer = () => {
     return (
         <>
             <div className='footer'>
                 <div className='footer-logo'>
-                    <section className='Hero'>
-                        <img
-                            className='footer-logo-img'
-                            src={process.env.PUBLIC_URL + '/images/mks.png'}
-                        />
-                        <ul>
-                            <li>
-                                <img
-                                    src={
-                                        process.env.PUBLIC_URL +
-                                        '/images/twitter.png'
-                                    }
-                                ></img>
-                            </li>
-                            <li>
-                                <img
-                                    src={
-                                        process.env.PUBLIC_URL +
-                                        '/images/github.png'
-                                    }
-                                ></img>
-                            </li>
-                            <li>
-                                <img
+                    <IconContext.Provider
+                        className='social-icons'
+                        value={{ color: 'rgb(143, 143, 143)', size: '1.4rem' }}
+                    >
+                        <section className='Hero'>
+                            <img
+                                className='footer-logo-img'
+                                src={process.env.PUBLIC_URL + '/images/mks.png'}
+                            />
+                            <ul>
+                                <li>
+                                    {/* <img
+                                        src={
+                                            process.env.PUBLIC_URL +
+                                            '/images/twitter.png'
+                                        }
+                                    ></img> */}
+                                    <SiTwitter />
+                                </li>
+                                <li>
+                                    {/* <SlSocialLinkedin /> */}
+                                    {/* <TiSocialLinkedinCircular /> */}
+                                    <a
+                                        target='_blank'
+                                        href='https://www.linkedin.com/company/ju-makerspace/'
+                                    >
+                                        <TfiLinkedin />
+                                    </a>
+                                    {/* <TfiLinkedin /> */}
+                                </li>
+                                <li>
+                                    {/* <img
+                                        src={
+                                            process.env.PUBLIC_URL +
+                                            '/images/github.png'
+                                        }
+                                    ></img> */}
+                                    <a
+                                        target='_blank'
+                                        href='https://github.com/ju-makerspace'
+                                    >
+                                        <SiGithub />
+                                    </a>
+                                </li>
+                                <li>
+                                    {/* <img
                                     src={
                                         process.env.PUBLIC_URL +
                                         '/images/insta.png'
                                     }
-                                ></img>
-                            </li>
-                            <li>
-                                <img
+                                ></img> */}
+                                    <a
+                                        target='_blank'
+                                        href='https://www.instagram.com/jumakerspace/'
+                                    >
+                                        <SiInstagram />
+                                    </a>
+                                </li>
+                                <li>
+                                    {/* <img
                                     src={
                                         process.env.PUBLIC_URL +
                                         '/images/discord.png'
                                     }
-                                ></img>
-                            </li>
-                        </ul>
-                    </section>
+                                ></img> */}
+                                    <IoMailSharp />
+                                    {/* <IoMailOutline className='social-icons' /> */}
+                                </li>
+                            </ul>
+                        </section>
+                    </IconContext.Provider>
 
                     <div className='sec-container'>
                         <section className='Domain'>
