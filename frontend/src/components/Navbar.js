3c824221dcfc89027264186d136ca5c0a918555d
Arpit Verma <arpitv970@gmail.com>
2022-11-24 15:30:34 +0530

    testing

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,23 +5,25 @@ const Navbar = () => {
     return (
         <navbar>
             <img src={process.env.PUBLIC_URL + '/images/mks.png'} />
-            <ul>
-                <li>
-                    <NavLink to='/'>Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/about'>About</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/featured'>Featured</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/mentors'>Mentors</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/alumni'>Alumni</NavLink>
-                </li>
-            </ul>
+            <div className='nav-links'>
+                <ul>
+                    <li>
+                        <NavLink to='/'>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/about'>About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/featured'>Featured</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/mentors'>Mentors</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/alumni'>Alumni</NavLink>
+                    </li>
+                </ul>
+            </div>
         </navbar>
     );
 };
