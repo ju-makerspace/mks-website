1ece96a10d097d291908ff1db8b971a03f11810d
Arpit Verma <arpitv970@gmail.com>
2023-02-16 20:16:41 +0530

    Updated Hero section

--- frontend/src/components/home/Hero.js
+++ frontend/src/components/home/Hero.js
@@ -1,4 +1,4 @@
-import '../../styles/desktop/home/Hero.css'
+import '../../styles/desktop/home/Hero.css';
 
 const Hero = () => {
     return (
@@ -6,10 +6,12 @@ const Hero = () => {
             <img
                 className='gear'
                 src={process.env.PUBLIC_URL + '/images/octicon_gear-16.png'}
+                alt='utilities'
             />
             <img
                 className='chip'
                 src={process.env.PUBLIC_URL + '/images/uil_circuit.png'}
+                alt='utilities'
             />
             <img
                 className='laptop'
@@ -17,6 +19,7 @@ const Hero = () => {
                     process.env.PUBLIC_URL +
                     '/images/icon-park-solid_code-laptop.png'
                 }
+                alt='utilities'
             />
             <div className='hero-text'>
                 Building community for <span className='grad1'>Everyone</span>
