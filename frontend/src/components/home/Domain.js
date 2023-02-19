c9d61f1082e3a9d43d02176c81e674274f9d296a
Arpit Verma <arpitv970@gmail.com>
2023-02-19 18:23:10 +0530

    changes in domains

--- frontend/src/components/home/Domain.js
+++ frontend/src/components/home/Domain.js
@@ -7,17 +7,20 @@ const Domain = () => {
                 Domains available in <span className='grad2'>Makerspace</span>
             </div>
             <div className='domain_list'>
+                <div className='domain_item'>IoT</div>
+                <div className='domain_item'>Electronics</div>
+                <div className='domain_item'>AI/ML</div>
                 <div className='domain_item'>Robotics</div>
-                <div className='domain_item'>Robotics</div>
-                <div className='domain_item'>Robotics</div>
-                <div className='domain_item'>Robotics</div>
-                <div className='domain_item'>Robotics</div>
-                <div className='domain_item'>Robotics</div>
+                <div className='domain_item'>S/W-Dev</div>
+                <div className='domain_item'>Graphics</div>
             </div>
             <div className='domain_desc'>
                 Creating cool stuffs, with amazing team{' '}
                 <div>
-                    <img src={process.env.PUBLIC_URL + '/images/idea.png'} />
+                    <img
+                        alt='idea'
+                        src={process.env.PUBLIC_URL + '/images/idea.png'}
+                    />
                 </div>
             </div>
         </div>
